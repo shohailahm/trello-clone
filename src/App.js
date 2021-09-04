0bde9b67d8f937ca76bb0b32a05142461032020f
shohail ahmed <shohail@cloudthing.com>
2021-09-04 12:04:44 +0530

    react dnd and header implimentation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import Header from "./components/header";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <DndProvider backend={HTML5Backend}>
+        <Header />
+      </DndProvider>
+    </div>
+  );
 }
 
 export default App;
